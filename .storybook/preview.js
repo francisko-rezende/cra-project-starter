ffa4f4ecc60bd9240d57e228250f98816af45b44
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-03-07 17:48:16 -0300

    feat: start using global styles in stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+import React from 'react';
+
+import GlobalStyles from '../src/styles/GlobalStyles'
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Story />
+      <GlobalStyles />
+    </>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
